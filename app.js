8ae811d69c3941a0011bfc7517fcce4ec8b8c4e9
Juuso <juuso.saltiola@gmail.com>
2019-03-15 11:15:33 +0000

    virheet korjattu

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+// Tämä on sovelluksen aloitus-skripti
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req,res) {
+    var q = url.parse(req.url, true);
+    var filename = "./pages" + q.pathname;
+  
+    fs.readFile(filename, function(err, data ) {
+        if (err)   {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+    
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+
+
+
+}).listen(8080);
+
+
